aca38fb32e9441328c6bc4931caa32305ba87d7d
giveupwwl <2363843340@qq.com>
2018-11-16 10:12:52 +0800

    首页加用户完成

--- public/back/js/index.js
+++ public/back/js/index.js
@@ -1,7 +1,7 @@
-$(function(){
- //初始化echarts_left实例
+$(function () {
+  //初始化echarts_left实例
   var echarts_left = echarts.init(document.querySelector(".echarts_left"));
-  
+
   //指定图表
   var option1 = {
     // 大标题
@@ -17,7 +17,7 @@ $(function(){
     },
     // 图例
     legend: {
-      data:['人数', "销量"]
+      data: ['人数', "销量"]
     },
     // x轴的数据
     xAxis: {
@@ -31,7 +31,7 @@ $(function(){
       // bar 表示柱状图,  line 表示折线图,  pie 表示饼图
       type: 'bar',
       data: [500, 1200, 390, 580, 800, 1000]
-    },{
+    }, {
       name: '销量',
       type: 'bar',
       data: [700, 1300, 490, 680, 700, 800]
@@ -41,8 +41,54 @@ $(function(){
   // 使用刚指定的配置项和数据显示图表。
   echarts_left.setOption(option1);
 
+  //初始化echarts_left实例
+  var echarts_right = echarts.init(document.querySelector(".echarts_right"));
+  option2 = {
+    title: {
+      text: '热门品牌销售',
+      subtext: '2018年11月',
+      x: 'center',
+      //配置文本样式
+      textStyle:{
+        color:"#e92322",
+        fontSize:25
+      }
+    },
+    tooltip: {
+      trigger: 'item',
+      formatter: "{a} <br/>{b} : {c} ({d}%)"
+    },
+    legend: {
+      orient: 'vertical',
+      left: 'left',
+      data: ['耐克', '阿迪', '阿迪王', '解放', '老北京']
+    },
+    series: [
+      {
+        name: '品牌销售',
+        type: 'pie',
+        radius: '55%',
+        center: ['50%', '60%'],
+        data: [
+          { value: 335, name: '耐克' },
+          { value: 310, name: '阿迪' },
+          { value: 234, name: '阿迪王' },
+          { value: 135, name: '解放' },
+          { value: 1548, name: '老北京' }
+        ],
+        itemStyle: {
+          emphasis: {
+            shadowBlur: 10,
+            shadowOffsetX: 0,
+            shadowColor: 'rgba(0, 0, 0, 0.5)'
+          }
+        }
+      }
+    ]
+  };
 
-
+   // 使用刚指定的配置项和数据显示图表。
+   echarts_right.setOption(option2);
 
 
 
